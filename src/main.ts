e3f2e56e8eb1cc3ad69449f457425714f33c0ea1
diffshare <diffshare@gmail.com>
2016-07-23 16:04:35 +0900

    angular2のバージョンをrc.4に上げる router-deprecatedを外す

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import {bootstrap} from '@angular/platform-browser-dynamic'
 import {App} from './app/app';
 import {HTTP_PROVIDERS} from "@angular/http";
-import {ROUTER_PROVIDERS} from "@angular/router";
 
 //noinspection TypeScriptValidateTypes
 bootstrap(App, [
-    ...HTTP_PROVIDERS,
-    ...ROUTER_PROVIDERS,
+    ...HTTP_PROVIDERS
 ]).catch(err => console.error(err));
